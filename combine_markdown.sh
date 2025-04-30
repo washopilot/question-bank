f398be5d5a212d89099d0342aa263609e3edba68
Fernando Chicaiza <washopilot@yahoo.com>
2025-04-30 08:17:43 -0500

    some OMVR-SIN

--- combine_markdown.sh
+++ combine_markdown.sh
@@ -10,8 +10,8 @@
 
 # Configuración inicial con valores por defecto
 DIR="markdown/unit1"
-CATEGORY="COM"
-TYPE="PRB" 
+CATEGORY="SIN"
+TYPE="OMVR" 
 OUTPUT_FILE="combined_markdown.md"
 
 # Procesar argumentos
